7618bce3d79c4d15f05c028212d0045b9fd26f03
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-29 05:43:23 +0300

    added tab to navigate between root pages

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 const Chat = () => {
   return (
     <View>
-      Chat
+      <Text>Chat</Text>
     </View>
   )
 }
