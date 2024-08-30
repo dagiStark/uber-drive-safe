951b98902b79659b3049f5530adc345209480d68
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-08-30 20:51:08 +0300

    added mock data to text nearby drivers

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/type";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const GoogleTextInput = ({
   icon,
@@ -13,7 +14,39 @@ const GoogleTextInput = ({
     <View
       className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
     >
-      <Text>GoogleTextInput</Text>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="where you want to go?"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: "relative",
+            shadowColor: "#d4d4d4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: '600',
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || 'white',
+            position: 'relative',
+            top: 0,
+            width: '100%',
+            borderRadius: 10,
+            shadowColor: '#d4d4d4',
+            zIndex: 99,
+
+          }
+        }}
+      />
     </View>
   );
 };
